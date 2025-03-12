e15cb7adec0d0780d34235230163371b73af3251
ArtificerArturo <198648779+ArtificerArturo@users.noreply.github.com>
2025-03-12 14:06:34 -0500

    discovered the showMixed flag on math.fraction, removed extraneous code

--- script.js
+++ script.js
@@ -3,6 +3,7 @@ function calculateFraction() {
     const denominatorLeftElement = document.querySelector("#denominatorLeft");
     const numeratorRightElement = document.querySelector("#numeratorRight")
     const denominatorRightElement = document.querySelector("#denominatorRight")
+
     const operatorElement = document.querySelector("#operator")
 
     const numeratorLeft = Math.round(numeratorLeftElement.value);
@@ -17,26 +18,12 @@ function calculateFraction() {
         resultElement.style.color = "red";
     } else if (operatorElement.value == "plus") {
         let result = math.add(math.fraction(numeratorLeft, denominatorLeft), math.fraction(numeratorRight, denominatorRight))
-        if (Number(result.n) > Number(result.d)) {
-            let wholeNumber = Number(result.n) % Number(result.d)
-            result = math.subtract(result, wholeNumber)
-            resultElement.textContent = `The answer is: ${wholeNumber} ${result.toFraction()}`;
-            resultElement.style.color = "black";
-        } else {
-            resultElement.textContent = `The answer is: ${result.toFraction()}`;
-            resultElement.style.color = "black";
-        }
+        resultElement.textContent = `The answer is: ${result.toFraction(true)}`;
+        resultElement.style.color = "black";
     } else {
         //if operator minus -- modify if add multiply or divide or something
         let result = math.subtract(math.fraction(numeratorLeft, denominatorLeft), math.fraction(numeratorRight, denominatorRight))
-        if (Number(result.n) > Number(result.d)) {
-            let wholeNumber = Number(result.n) % Number(result.d)
-            result = math.subtract(result, wholeNumber)
-            resultElement.textContent = `The answer is: ${wholeNumber} ${result.toFraction()}`;
-            resultElement.style.color = "black";
-        } else {
-            resultElement.textContent = `The answer is: ${result.toFraction()}`;
-            resultElement.style.color = "black";
-        }
+        resultElement.textContent = `The answer is: ${result.toFraction(true)}`;
+        resultElement.style.color = "black";
     }
 }
