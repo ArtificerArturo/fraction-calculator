e57851a47c9885eafe3dcfbaa5d1b48147955e7f
ArtificerArturo <198648779+ArtificerArturo@users.noreply.github.com>
2025-03-13 12:43:07 -0500

    Copied from mometrix-calculator: added multiplication and division, improved result display, improved input logic

--- script.js
+++ script.js
@@ -1,29 +1,75 @@
+const resultWholeNumber = document.querySelector("#resultWholeNumber")
+const resultNumerator = document.querySelector("#resultNumerator")
+const resultDenominator = document.querySelector("#resultDenominator")
+const resultFractionBar = document.querySelector("#resultFractionBar")
+const resultElement = document.querySelector("#result");
+
+const numeratorLeftElement = document.querySelector("#numeratorLeft");
+const denominatorLeftElement = document.querySelector("#denominatorLeft");
+const numeratorRightElement = document.querySelector("#numeratorRight")
+const denominatorRightElement = document.querySelector("#denominatorRight")
+
+const operatorElement = document.querySelector("#operator")
+
 function calculateFraction() {
-    const numeratorLeftElement = document.querySelector("#numeratorLeft");
-    const denominatorLeftElement = document.querySelector("#denominatorLeft");
-    const numeratorRightElement = document.querySelector("#numeratorRight")
-    const denominatorRightElement = document.querySelector("#denominatorRight")
-
-    const operatorElement = document.querySelector("#operator")
-
-    const numeratorLeft = Math.round(numeratorLeftElement.value);
-    const denominatorLeft = Math.round(denominatorLeftElement.value);
+    //round input to nearest integer. could change this to just reject non-integers
+    const numeratorLeft = Math.round(numeratorLeftElement.value)
+    const denominatorLeft = Math.round(denominatorLeftElement.value)
     const numeratorRight = Math.round(numeratorRightElement.value)
     const denominatorRight = Math.round(denominatorRightElement.value)
 
+    //display rounded value back to user
+    numeratorLeftElement.value = numeratorLeft
+    denominatorLeftElement.value = denominatorLeft
+    numeratorRightElement.value = numeratorRight
+    denominatorRightElement.value = denominatorRight
+
     const resultElement = document.querySelector("#result");
 
-    if (isNaN(denominatorLeft) || isNaN(denominatorRight) || isNaN(numeratorLeft) || isNaN(numeratorRight) || denominatorLeft <= 0 || denominatorRight <= 0) {
-        resultElement.textContent = "Please enter valid numbers. Denominators cannot be negative or zero, and all fields must contain an integer.";
+    if (isNaN(denominatorLeft) || isNaN(denominatorRight) ||
+        denominatorLeft <= 0 || denominatorRight <= 0) {
+        resultWholeNumber.textContent = "Please enter valid numbers. Denominators cannot be negative or zero, and all fields must contain integers.";
         resultElement.style.color = "red";
+        resultNumerator.textContent = ""
+        resultDenominator.textContent = ""
+        resultFractionBar.style.border = ""
     } else if (operatorElement.value == "plus") {
-        let result = math.add(math.fraction(numeratorLeft, denominatorLeft), math.fraction(numeratorRight, denominatorRight))
-        resultElement.textContent = `The answer is: ${result.toFraction(true)}`;
-        resultElement.style.color = "black";
+        displayFraction(math.add(math.fraction(numeratorLeft, denominatorLeft), math.fraction(numeratorRight, denominatorRight)))
+    } else if (operatorElement.value == "minus") {
+        displayFraction(math.subtract(math.fraction(numeratorLeft, denominatorLeft), math.fraction(numeratorRight, denominatorRight)))
+    } else if (operatorElement.value == "multiply") {
+        displayFraction(math.multiply(math.fraction(numeratorLeft, denominatorLeft), math.fraction(numeratorRight, denominatorRight)))
     } else {
-        //if operator minus -- modify if add multiply or divide or something
-        let result = math.subtract(math.fraction(numeratorLeft, denominatorLeft), math.fraction(numeratorRight, denominatorRight))
-        resultElement.textContent = `The answer is: ${result.toFraction(true)}`;
-        resultElement.style.color = "black";
+        displayFraction(math.divide(math.fraction(numeratorLeft, denominatorLeft), math.fraction(numeratorRight, denominatorRight)))
+    }
+}
+
+function displayFraction(result) {
+    resultElement.style.color = "black";
+
+    let resultArray = []
+    let fractionArray = []
+
+    if (result.n > result.d && result.d != 1) {
+        //mixed number
+        resultArray = result.toFraction(true).split(" ")
+        resultWholeNumber.textContent = `Answer: ${resultArray[0]}`
+        fractionArray = resultArray[1].split("/")
+        resultNumerator.textContent = fractionArray[0]
+        resultDenominator.textContent = fractionArray[1]
+        resultFractionBar.style.border = "1px solid black"
+    } else if (result.n % result.d == 0) {
+        //whole number only
+        resultWholeNumber.textContent = `Answer: ${result.toFraction(true)}`
+        resultNumerator.textContent = ""
+        resultDenominator.textContent = ""
+        resultFractionBar.style.border = ""
+    } else {
+        //fraction only
+        fractionArray = result.toFraction(true).split("/")
+        resultWholeNumber.textContent = "Answer: "
+        resultNumerator.textContent = fractionArray[0]
+        resultDenominator.textContent = fractionArray[1]
+        resultFractionBar.style.border = "1px solid black"
     }
 }
